874f1d3696766f59aa83d84961acea0096130183
Ubuntu <ubuntu@ip-172-31-0-62.ap-northeast-1.compute.internal>
2020-10-12 14:42:47 +0000

    add header and footer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  
+  def help
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+  
   devise_for :users ,controllers: {
-    sessions: 'users/sessions',
-    registrations: 'users/registrations'
+    sessions:       'users/sessions',
+    registrations:  'users/registrations'
   }
+  
   root 'homes#top'
+  
+  get '/help' ,   to: 'homes#help'
+  get '/about',   to: 'homes#about'
+  get '/contact', to: 'homes#contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
